b42bf490a54ebf7aca098cb21e4f8f3b8b9e97ad
Maxim <work.maydjin@gmail.com>
2012-09-03 16:25:28 +0400

    initial commit

---
+++ src/com/epam/AnotherSearch/AnotherSearchActivity.java
@@ -0,0 +1,13 @@
+package com.epam.AnotherSearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherSearchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
