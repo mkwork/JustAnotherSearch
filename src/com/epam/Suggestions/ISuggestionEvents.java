8f23a296c16a4b95283786553f3a92ba933dfc01
Maxim <work.maydjin@gmail.com>
2012-10-04 16:09:49 +0400

    Obsolete Suggestions package was removed

--- src/com/epam/Suggestions/ISuggestionEvents.java
+++
@@ -1,9 +0,0 @@
-package com.epam.Suggestions;
-
-public interface ISuggestionEvents {
-	
-boolean OnReloadStart();
-boolean OnReloadFinished();
-boolean OnSuggestionLoaded(ISuggestion suggestion);
-boolean OnSuggestionPreload(ISuggestion suggestion);
-}
